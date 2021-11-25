3439343915f78a52b6881ba6584238f83f97b153
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-25 19:47:49 +0100

    bonus complete

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 var root = new Vue ({
   el:"#root",
-  data: {
-  message:"Ciao!"
+  data:{
+    message:"Hello Vue",
+    mieClassi : "text-red",
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+           ],
+    imageSrc : "img/colosseo-roma-storia.jpg"
+    
+    
+  },
+
+  methods : {
+    TextColor: function(){
+      if(this.mieClassi == 'text-blue') {
+				this.mieClassi = 'text-red';
+			} else {
+				this.mieClassi = 'text-blue';
+			}
+    }
+
+  },
+
   
-   }
-   
 
+ 
+  
 
-
-
-
-
-
+  
+  
 
 });
 
